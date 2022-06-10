83c1e43b58eec8f473adc3756647e04145579753
capy-larit <larissa.laritt@icloud.com>
2022-06-09 22:18:35 -0300

    enhancement(tests): Adiciona testes para a função decodificar
    - Adiciona teste com código de palavra simples
    - Adiciona teste com código de palavras com espaço

--- tests/test_codigo_morse.py
+++ tests/test_codigo_morse.py
@@ -1,4 +1,4 @@
-from codigo_morse import codificar
+from codigo_morse import codificar, decodificar
 
 
 def test_codificar_ola_deve_retornar_codigo_correto():
@@ -9,3 +9,15 @@ def test_codificar_ola_deve_retornar_codigo_correto():
 def test_codificar_frase_com_espaco():
     esperado = '--- .-.. .- / --- .-.. .- / --- .-.. .-'
     assert codificar('ola ola ola') == esperado
+
+
+def test_decodificar_ola():
+    esperado = 'ola'
+    assert decodificar('--- .-.. .-') == esperado
+
+
+def test_decodificar_frase_com_espaco():
+    esperado = 'ola ola ola'
+    assert (
+        decodificar('--- .-.. .- / --- .-.. .- / --- .-.. .-') == esperado
+    )
